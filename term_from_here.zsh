3b9867fa570a94d86b222b6e48197cf8518681ee
lain <tbkopera@proton.me>
2025-04-26 02:13:04 +0100

    spawned terminals can now spawn their own terminals

--- term_from_here.zsh
+++ term_from_here.zsh
@@ -1,6 +1,11 @@
 open_terminal_in_same_dir() {
     local terminal=$(ps -p $(ps -p $$ -o ppid=) -o args=)
+
+    if [[ "$terminal" == *"--working-directory"* ]]; then
+        eval "$terminal" &> /dev/null &
+    else
     eval "$terminal --working-directory="$PWD"" &
+    fi
 }
 zle -N open_terminal_in_same_dir
 bindkey '^[\' open_terminal_in_same_dir
