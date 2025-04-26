e7242b5a3fb5d1a8ffb534458b8fe19cfbcf32f5
lain <tbkopera@proton.me>
2025-04-26 01:42:38 +0100

    quickfix for async

--- term_from_here.zsh
+++ term_from_here.zsh
@@ -1,5 +1,5 @@
 function open-terminal-in-same-dir() {
-  $TERM --working-directory="$PWD"
+  $TERM --working-directory="$PWD" &
 }
 zle -N open-terminal-in-same-dir
 bindkey '^[\' open-terminal-in-same-dir
