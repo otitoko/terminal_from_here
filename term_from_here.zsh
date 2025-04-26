c166911a7db7a4f99ba007e961aff41b81bff7e5
lain <tbkopera@proton.me>
2025-04-26 01:26:10 +0100

    quickfix because kestrel 2 lazy to fork his own code

--- term_from_here.zsh
+++ term_from_here.zsh
@@ -1,5 +1,5 @@
 function open-terminal-in-same-dir() {
-  alacritty --working-directory="$PWD"
+  $TERM --working-directory="$PWD"
 }
 zle -N open-terminal-in-same-dir
 bindkey '^[\' open-terminal-in-same-dir
